3810d1e1d622fcab98455cd757c9c459561e736f
Arun-kumar-19 <118425866+Arun-kumar-19@users.noreply.github.com>
2023-02-14 14:42:50 +0530

    changes

---
+++ superstar.js
@@ -0,0 +1,10 @@
+// print superstar for add number
+// print rajini for event number
+
+for (let i = 1; i < 101; i++) {
+  if (i % 2 == 1) {
+    console.log(i + ".Rajini");
+  } else {
+    console.log(i + ".Superstar");
+  }
+}
