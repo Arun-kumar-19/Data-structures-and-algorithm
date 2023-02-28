db9aef35e2f06edf425cf35f889fb3f995d8122c
Arun-kumar-19 <118425866+Arun-kumar-19@users.noreply.github.com>
2023-02-28 08:11:22 +0530

    commit

--- ascendingOrder.js
+++ ascendingOrder.js
@@ -1,2 +1,10 @@
 // Write code to arrange the array in ascending order
 // for [5, 10, 1, 2, 8] output is[1, 2, 5, 8, 10]
+
+let arr = [5, 10, 1, 2, 8];
+
+arr.sort(function (a, b) {
+  return a - b;
+});
+
+console.log(arr);
--- printX.js
+++ printX.js
@@ -1,21 +1,21 @@
 let n = 4;
 
 function strair(n) {
-  let str = "";
+  let str1 = "";
   for (let i = 1; i <= n; i++) {
-    str = i + " ";
+    str1 = i;
     // console.log(str);
   }
-  return str;
+  return str1;
 }
 
 function detair(n) {
-  let str = "";
-  for (let i = n; i <= 1; i--) {
-    str = i + " ";
+  for (let i = n; i >= 1; i--) {
+    let str2 = "";
+    str2 = i + " ";
     // console.log(str);
+    return str2;
   }
-  return str;
 }
 
 function space(n) {
@@ -29,10 +29,11 @@ function space(n) {
 
 for (let i = 1; i <= n; i++) {
   let out1 = space(i) + strair(i);
-  console.log(out1);
+  let out2 = space(n + i + (n - i)) + strair(i);
+  console.log(out1 + out2);
 }
 
-for (let i = 1; i <= n; i++) {
-  let out2 = space(n - i) + detair(n - i);
-  console.log(out2);
-}
+// for (let j = n - 1; j >= 1; j--) {
+//   let out2 = space(j) + detair(j);
+//   console.log(out2);
+// }
